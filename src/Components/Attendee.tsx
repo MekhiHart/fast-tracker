330f07cb03c214a7984c40ab9c073d736352d264
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2024-03-13 15:16:36 -0700

    added visual feedback for attendee when successfully registered

--- src/Components/Attendee.tsx
+++ src/Components/Attendee.tsx
@@ -16,14 +16,18 @@ export default function Attendee(props:any){
     },[])
 
     const registerStatus = (
-        <h2 className="attendee--status">Attendee has not attended</h2>
+        <h2 className="attendee--status red">{firstname} has not attended</h2>
     )
+
+    const attendedStatus = <h2 className="attendee--status green">{firstname} Registered!</h2>
+
     return(
         <>
            {userData.hasAttended && <h2>Event Points: {eventpoints}</h2>}
-            {!userData.hasAttended && registerStatus }
             <div id="qr-code"/>
             <QRCodeSVG value={id} className="qr--code"/>
+            {!userData.hasAttended ? registerStatus : attendedStatus }
+
         </>
     )
 }
